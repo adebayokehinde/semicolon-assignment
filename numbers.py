b1a5b1966dd95156ede8f145600903584df93b88
adebayo kehinde <adebayosamuel6667@gmail.com>
2024-10-18 18:33:19 +0100

    semicolon-assignment

---
+++ numbers.py
@@ -0,0 +1,11 @@
+number1 = ()
+number2 = ()
+number3 = ()
+
+input(number1), (number2), (number3);
+number1 = number1 % 100
+reminder =- number1 // 100
+resultTwo = reminder // 10
+resultThree = reminder // 10
+sum = resultOne + resultTwo + resultThree
+print("The sum of the digit is:", sum);  
---
+++ pyton class/area.py
@@ -0,0 +1,2 @@
+number1 = int(input("enter number of radius:"))
+print(3.142* number1 * number1) 
