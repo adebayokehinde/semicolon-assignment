c6e49b574c28942ba01f51b4aca32feff88837ac
adebayo kehinde <adebayosamuel6667@gmail.com>
2024-11-01 00:54:10 +0100

    semicolon-assignment

---
+++ pythonassignment/increasingorder.py
@@ -0,0 +1,9 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter third number: "))
+num3 = float(input("Enter third number: "))
+if num1 > num2 and num2 > num3:
+        print("decreasing  order,")
+elif num1 < num2 and num2 < num3:
+        print("increasing order,")
+else:
+        print("not increasing or decreasing,")
