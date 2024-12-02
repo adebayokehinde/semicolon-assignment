760a36e559c6413005fa187e37cb3973540239aa
adebayo kehinde <adebayosamuel6667@gmail.com>
2024-12-02 15:06:03 +0100

    semicolon-assignment

--- phasegateone/taskone.py
+++ phasegateone/taskone.py
@@ -1,24 +1,23 @@
 '''
-Pseudocode
-stage1
-1.prompt user to enter two integer  under 100
-2.collect numbers
-3.save
-4.sum up both numbers
-5.is the sum is correct
-6.print true
-7.else
-8.print false
+prompt user to enter number 
+collect 
+save as userInput
+create a variable to store the guessed number
+store as random_number
+create a variable to store the sum of the random_number
+store as sum_of_random_number
+determine if the userInput == sum_of_random_number
+print "True"
+else
+print "False"
 '''
-number1 = int(input("Enter first number under 100  "))
-number2 = int(input("Enter second number under 100  "))
 
+import random
+userInput = int(input("Guess the sum of two numbers:"))
+random_number = random.randint(1 , 100)
+sum_of_random_number = random_number + random_number
 
-sum = number1 + number2
-
-if sum == number1 + number2:
-	print("true")
+if userInput == sum_of_random_number:
+	print("True")
 else:
-	print("false")
-
-print('the sum of two integer', sum)
+	print("False")
---
+++ phasegateone/tasktwo.py
@@ -0,0 +1,9 @@
+user_input = int(input("Enter number between 0 - 1000:"))
+first_number = user_input // 1000
+first_reminder = user_input % 1000
+second_number = first_reminder // 100
+second_reminder = first_reminder % 100
+third_number = second_reminder // 10
+third_reminder = second_reminder % 10
+sum_of_numbers = first_number + second_number + third_number + third_reminder
+print(sum_of_numbers)
